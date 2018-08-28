793f6162df2650bce78686c44c3fbe740a0b1e6a
naphattharawat <naphattharawat@gmail.com>
2018-08-28 08:07:59 +0700

    change api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3001',
   apiConsents: 'http://api.consent.moph.go.th/v1',
-  apiH4u: 'http://localhost:3000'
+  apiH4u: 'https://h4u.moph.go.th/api/officer/v1'
 };
