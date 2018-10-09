fa5513e4111263af358162917b43ae586e81e81f
NongMinty <nutsuke.m@gmail.com>
2018-10-09 16:44:12 +0700

    fix timestamp to thai date

--- src/app/to-thai-date.pipe.ts
+++ src/app/to-thai-date.pipe.ts
@@ -9,11 +9,8 @@ export class ToThaiDatePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     moment.locale('th');
-    if (moment(value, 'YYYY-MM-DD').isValid() && value.length <= 10) {
+    if (moment(value, 'YYYY-MM-DD').isValid()) {
       const thaiDate = `${moment(value).format('DD MMM')} ${moment(value).get('year') + 543}`;
-      return thaiDate;
-    } else if (moment(value, 'x').isValid()) {
-      const thaiDate = `${moment(value, 'x').format('DD MMM')} ${moment(value, 'x').get('year') + 543}`;
       return thaiDate;
     } else {
       return '-';
